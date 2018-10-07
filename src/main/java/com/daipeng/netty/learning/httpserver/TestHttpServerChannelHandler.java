f043424734a2335e9aff19e74e49b587f69d6026
daipeng <daipeng@cloud-young.com>
2018-10-07 15:31:53 +0800

    rename package name

--- src/main/java/com/daipeng/netty/learning/httpserver/TestHttpServerChannelHandler.java
+++ src/main/java/com/daipeng/netty/learning/httpserver/TestHttpServerChannelHandler.java
@@ -1,4 +1,4 @@
-package com.daipeng.netty.learning.helloworld;
+package com.daipeng.netty.learning.httpserver;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
--- src/main/java/com/daipeng/netty/learning/httpserver/TestServer.java
+++ src/main/java/com/daipeng/netty/learning/httpserver/TestServer.java
@@ -1,4 +1,4 @@
-package com.daipeng.netty.learning.helloworld;
+package com.daipeng.netty.learning.httpserver;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
